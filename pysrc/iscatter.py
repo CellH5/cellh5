79bfdf49fe82f3a98e39241f78b8eab701ecd0e6
Christoph Sommer <christoph.sommer@imba.oeaw.ac.at>
2014-02-19 16:14:09 +0100

    Improved iscatte with 3rd dimension C

--- pysrc/iscatter.py
+++ pysrc/iscatter.py
@@ -6,6 +6,10 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg
 
 from matplotlib import rcParams
+import resources
+for t in rcParams.keys():
+    print t
+     
 
 rcParams['font.family'] = 'sans-serif'
 rcParams['font.sans-serif'] = ['Arial']
@@ -16,17 +20,19 @@ rcParams['pdf.fonttype'] = 42
 # rcParams['xtick.major.width'] = 2
 # rcParams['ytick.major.width'] = 2
 # rcParams['text.color'] = 'white'
-# rcParams['xtick.color'] = 'white'
-# rcParams['ytick.color'] = 'white'
+rcParams['xtick.color'] = 'white'
+rcParams['ytick.color'] = 'white'
+rcParams['ytick.labelsize'] = 14
+rcParams['xtick.labelsize'] = 14
+rcParams['axes.labelsize'] = 18
 
-from matplotlib.widgets import Lasso
+rcParams['axes.labelcolor'] = 'white'
+
+from matplotlib.widgets import RectangleSelector
 from matplotlib.colors import colorConverter
-from matplotlib.collections import RegularPolyCollection, CircleCollection
-from matplotlib import path
-from pylab import cm 
-
-from numpy import nonzero
-from numpy.random import rand
+from matplotlib.collections import CircleCollection
+from matplotlib import cm 
+from matplotlib import colors
 
 import cellh5, numpy
 
@@ -42,65 +48,8 @@ class DataPoint(object):
         else: 
             self.color = self.colorout
 
-class LassoManager(QtCore.QObject):
-    selectionChanged = QtCore.pyqtSignal(numpy.ndarray)
-    def __init__(self, ax, data):
-        super(QtCore.QObject, self).__init__()
-        self.axes = ax
-        self.canvas = ax.figure.canvas
-        self.data = data
-        
-        self.axes.clear()
-        self.Nxy = len(data)
-
-        self.facecolors = [d.color for d in data]
-        self.xys = [(d.x, d.y) for d in data]
-        
-        self.collection = CircleCollection(
-            sizes=(20,),
-            facecolors=self.facecolors,
-            offsets = self.xys,
-            transOffset = ax.transData)
-
-        ax.add_collection(self.collection)
-
-        self.cid = self.canvas.mpl_connect('button_press_event', self.onpress)
-        self.selectionChanged.connect(self.update_colors)
-
-    def callback(self, verts):
-        print 'callback'
-        p = path.Path(verts)
-        ind = p.contains_points(self.xys)
-        self.selectionChanged.emit(ind)
-        
-    def update_colors(self, ind):
-        facecolors = self.collection.get_facecolors()
-        self.facecolors = facecolors.copy()
-        ids =  [self.data[k].ref for k in range(len(ind)) if ind[k]] 
-        print ind.sum(), 'objects selected with refs', ids
-        for i in range(len(self.xys)):
-            if ind[i]:
-                facecolors[i] = DataPoint.colorin
-            else:
-                facecolors[i] = DataPoint.colorout
-                
-        self.canvas.draw_idle()
-        self.canvas.widgetlock.release(self.lasso)
-        del self.lasso
-        
-    def onpress(self, event):
-        if self.canvas.widgetlock.locked(): 
-            return
-        elif event.inaxes is None: 
-            return
-        elif event.button != 1:
-            return 
-        self.lasso = Lasso(event.inaxes, (event.xdata, event.ydata), self.callback)
-        # acquire a lock on the widget drawing
-        self.canvas.widgetlock(self.lasso)
-
-class MainWindow(QtGui.QMainWindow):
-    def __init__(self, widget_1, widget_2):  
+class IScatter(QtGui.QMainWindow):
+    def __init__(self, scatter, image):  
         QtGui.QMainWindow.__init__(self)
         self.setWindowTitle('Test')
         
@@ -108,32 +57,36 @@ class MainWindow(QtGui.QMainWindow):
         self.setCentralWidget(self.main_widget)
         
         layout = QtGui.QHBoxLayout()
-        layout.addWidget(widget_1)
-        layout.addWidget(widget_2)
+        layout.addWidget(scatter)
+        layout.addWidget(image)
         self.main_widget.setLayout(layout)  
         
         #widget_1.canvas.setParent(widget_1)
-        widget_1.canvas.setFocusPolicy(QtCore.Qt.ClickFocus)
-        widget_1.canvas.setFocus()      
+        scatter.canvas.setFocusPolicy(QtCore.Qt.ClickFocus)
+        image.canvas.setFocus()      
         
         self.apply_css("iscatter.css")
         
     def apply_css(self, css_file):
-         qss_file = QtCore.QFile(css_file)
-         qss_file.open(QtCore.QFile.ReadOnly);
-         css = QtCore.QLatin1String(qss_file.readAll());
-         self.setStyleSheet(css);    
+        qss_file = QtCore.QFile(css_file)
+        qss_file.open(QtCore.QFile.ReadOnly);
+        css = QtCore.QLatin1String(qss_file.readAll());
+        self.setStyleSheet(css);    
     
 class IScatterWidget(QtGui.QWidget):
     image_changed = QtCore.pyqtSignal(numpy.ndarray)
     def __init__(self, parent=None):   
         QtGui.QWidget.__init__(self, parent)
         self.figure = Figure()
+        self.figure.patch.set_alpha(0)
         self.canvas = FigureCanvasQTAgg(self.figure)
         self.axes = self.figure.add_subplot(111)
 
-        self.lasso = LassoManager(self.axes, [])
-        self.lasso.selectionChanged.connect(self.update_image)
+        self.selector = RectangleSelector(self.axes, self.selector_cb,
+                                       drawtype='box', useblit=True,
+                                       button=[1,],
+                                       minspanx=5, minspany=5,
+                                       spancoords='pixels')
         
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.canvas)
@@ -150,6 +103,18 @@ class IScatterWidget(QtGui.QWidget):
         axis_selector_layout.addWidget(QtGui.QLabel("Y:"))
         self.axis_y_cmb = QtGui.QComboBox(self)
         axis_selector_layout.addWidget(self.axis_y_cmb)
+        
+        axis_selector_layout.addStretch()
+        
+        axis_selector_layout.addWidget(QtGui.QLabel("C:"))
+        self.axis_c_chk = QtGui.QCheckBox(self)
+        self.axis_c_chk.setChecked(False)
+        self._last_c_dim = 0
+        self.axis_c_cmb = QtGui.QComboBox(self)
+        self.axis_c_cmb.setEnabled(False)
+        axis_selector_layout.addWidget(self.axis_c_chk)
+        axis_selector_layout.addWidget(self.axis_c_cmb)
+        
         axis_selector.setLayout(axis_selector_layout)
 
         layout.addWidget(axis_selector)
@@ -157,8 +122,34 @@ class IScatterWidget(QtGui.QWidget):
         self.setLayout(layout)
         
         self.canvas.mpl_connect('scroll_event', self.mouse_wheel_zoom)
+
         
-    def mouse_wheel_zoom(self, event, base_scale=1.1):
+    def selector_cb(self, epress, erelease):
+        x_min, x_max = min(epress.xdata, erelease.xdata), max(epress.xdata, erelease.xdata)
+        y_min, y_max = min(epress.ydata, erelease.ydata), max(epress.ydata, erelease.ydata)
+        
+        ind = numpy.array([True if ((x > x_min) and 
+                                    (x < x_max) and
+                                    (y > y_min) and 
+                                    (y < y_max)) 
+                                    else False for x, y in self.xys])
+        
+        edgecolors = self.collection.get_edgecolors()
+        facecolors = self.collection.get_facecolors()
+        
+        ids =  [self.data[k].ref for k in range(len(ind)) if ind[k]] 
+        print ind.sum(), 'objects selected with refs', ids
+        for i in range(len(self.xys)):
+            if ind[i]:
+                edgecolors[i] = DataPoint.colorin
+            else:
+                edgecolors[i] = facecolors[i]
+                
+        self.canvas.draw()
+        self.update_image(ind)
+        
+        
+    def mouse_wheel_zoom(self, event, base_scale=1.2):
         if event.key is None or not event.key.startswith('control'):
             return
         cur_xlim = self.axes.get_xlim()
@@ -189,7 +180,7 @@ class IScatterWidget(QtGui.QWidget):
     def update_image(self, ind):
         self.ind = ind
         ids =  [self.data[k].ref for k in range(len(ind)) if ind[k]]       
-        img = self.ch5_pos.get_gallery_image_matrix(ids, (10,10))
+        img = self.ch5_pos.get_gallery_image_matrix(ids, (10,12))
         self.image_changed.emit(img)
         
     def set_data(self, data_matrix, data_names, x_dim, y_dim, data_ch5_idx, ch5_pos):
@@ -199,11 +190,13 @@ class IScatterWidget(QtGui.QWidget):
         self.data_ch5_idx = data_ch5_idx
         self.x_dim = x_dim
         self.y_dim = y_dim
+        self.c_dim = None
         self.ind = [False]*data_matrix.shape[0]
         
         for dn in data_names:
             self.axis_x_cmb.addItem(dn)
             self.axis_y_cmb.addItem(dn)
+            self.axis_c_cmb.addItem(dn)
             
         self.update_axis()
         
@@ -212,6 +205,8 @@ class IScatterWidget(QtGui.QWidget):
         
         self.axis_x_cmb.currentIndexChanged.connect(self.axis_x_changed)
         self.axis_y_cmb.currentIndexChanged.connect(self.axis_y_changed)
+        self.axis_c_cmb.currentIndexChanged.connect(self.axis_c_changed)
+        self.axis_c_chk.stateChanged.connect(self.axis_c_toggled)
          
     def axis_x_changed(self, x_dim):
         self.x_dim = x_dim
@@ -221,11 +216,54 @@ class IScatterWidget(QtGui.QWidget):
         self.y_dim = y_dim
         self.update_axis()
         
+    def axis_c_changed(self, c_dim):
+        self.c_dim = c_dim
+        self.update_axis()
+        
+    def axis_c_toggled(self, state):
+        if not state:
+            self._last_c_dim = self.c_dim
+            self.c_dim = None
+            self.axis_c_cmb.setEnabled(False)
+        else:
+            self.c_dim = self._last_c_dim
+            self.axis_c_cmb.setEnabled(True)
+        
+        self.update_axis()
+        
     def update_axis(self):
-        self.data = [DataPoint(xy[0], xy[1], self.data_ch5_idx[i], self.ind[i]) 
+        self.axes.clear()
+        self.data = [DataPoint(xy[0], xy[1], self.data_ch5_idx[i], False) 
                      for i, xy in enumerate(self.data_matrix[:, [self.x_dim, self.y_dim]])]
-        self.lasso = LassoManager(self.axes, self.data)
-        self.lasso.selectionChanged.connect(self.update_image)
+        self.Nxy = len(self.data)
+
+        if self.c_dim is None:
+            facecolors = [d.color for d in self.data]
+            edgecolors = [d.color for d in self.data]
+        else:
+            f_0_min = self.data_matrix[:, self.c_dim].min()
+            f_0_max = self.data_matrix[:, self.c_dim].max()
+            cNorm  = colors.Normalize(vmin=f_0_min, vmax=f_0_max)
+            scalarMap = cm.ScalarMappable(norm=cNorm, cmap=cm.jet)
+            facecolors = [scalarMap.to_rgba(c) for c in self.data_matrix[:, self.c_dim]]
+            edgecolors = [scalarMap.to_rgba(c) for c in self.data_matrix[:, self.c_dim]]
+            
+        self.xys = [(d.x, d.y) for d in self.data]
+        
+        for i in range(len(self.xys)):
+            if self.ind[i]:
+                edgecolors[i] = DataPoint.colorin
+            
+        
+        self.collection = CircleCollection(
+            sizes=(22,),
+            facecolors=facecolors,
+            edgecolors=edgecolors,
+            offsets = self.xys,
+            transOffset = self.axes.transData)
+
+        self.axes.add_collection(self.collection)
+        
         self.update_axis_lims()
         self.canvas.draw()
         
@@ -237,14 +275,18 @@ class IScatterWidget(QtGui.QWidget):
         
         self.axes.set_xlim(f_0_min, f_0_max)
         self.axes.set_ylim(f_1_min, f_1_max)
+        self.axes.set_xlabel(self.data_names[self.x_dim])
+        self.axes.set_ylabel(self.data_names[self.y_dim])
         
 class SimpleMplImageViewer(QtGui.QWidget):
     def __init__(self, parent=None):   
         QtGui.QWidget.__init__(self, parent)
         self.figure = Figure()
+        self.figure.patch.set_alpha(0)
         self.canvas = FigureCanvasQTAgg(self.figure)
-        self.axes = self.figure.add_subplot(111)
+        self.axes = self.figure.add_axes([0, 0, 1, 1])
         self.axes.axis('off')
+        self.axes.set_frame_on(False)
         
         layout = QtGui.QHBoxLayout()
         layout.addWidget(self.canvas)
@@ -257,7 +299,8 @@ class SimpleMplImageViewer(QtGui.QWidget):
 
 
 def start_qt_event_loop():
-    app = QtGui.QApplication(sys.argv)     
+    app = QtGui.QApplication(sys.argv)
+    app.setStyle("plastique")     
     return app
     
 if __name__ == "__main__":
@@ -266,18 +309,26 @@ if __name__ == "__main__":
     ch5_pos = ch5_file.current_pos
      
     features = ch5_pos.get_object_features()
-    times = ch5_pos.get_all_time_idx() == 1
-    features = features[times, :]
+    times = ch5_pos.get_all_time_idx()
+    times_idx = times < 3
+    features = features[times_idx, :]
+    
+    center_x = ch5_pos.get_center(times_idx)['x']
+    center_y = ch5_pos.get_center(times_idx)['y']
+    predictions = ch5_pos.get_class_prediction()[times_idx]['label_idx']
+    
+    features = numpy.c_[features, times[times_idx], center_x, center_y, predictions]
+    
  
     data_names = ch5_file.object_feature_def()
     
     iscatter = IScatterWidget()
-    iscatter.set_data(features, data_names, 222, 236, numpy.nonzero(times)[0], ch5_pos)
+    iscatter.set_data(features, data_names + ['Time', 'Center x', 'Center y', 'Classificaiton'], 222, 236, numpy.nonzero(times_idx)[0], ch5_pos)
     
     image_viewer = SimpleMplImageViewer()
     iscatter.image_changed.connect(image_viewer.show_image)
     
-    mw = MainWindow(iscatter, image_viewer)
+    mw = IScatter(iscatter, image_viewer)
     mw.show()
     app.exec_()
     
