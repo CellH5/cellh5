686c3051d75fc79b6e6ecec66a171ce77424c160
Christoph Sommer <christoph.sommer@imba.oeaw.ac.at>
2015-01-09 15:34:36 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -13,15 +13,14 @@ __author__ = 'rudolf.hoefler@gmail.com'
 import sys
 sys.path.append('pysrc')
 
-
 from distutils.core import setup
 import cellh5
 
 setup(name='cellh5',
       version = cellh5,
       description = 'module for easy acces of cellh5 files',
-      author = 'Rudolf Hoefler',
-      author_email = 'rudolf.hoefler@gmail.com',
+      author = 'Christoph Sommer, Rudolf Hoefler',
+      author_email = 'christoph.sommer@imba.oeaw.ac.at, rudolf.hoefler@gmail.com',
       license = 'LGPL',
       url = 'http://cellh5.org',
       package_dir = {'': 'pysrc'},
